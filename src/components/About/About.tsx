a08022dfcb93c13c74b70769f234c491cc52b6f5
darkeris345 <dimuxxx72@gmail.com>
2024-03-20 10:34:36 +0200

    changing the path to pdf

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,5 +1,6 @@
 import { Button } from "react-bootstrap";
 import "./About.scss";
+import pdf from "../../assets/resume/Dmitrij_Purynzin_resume.pdf";
 
 const About: React.FC = () => {
   return (
@@ -7,7 +8,7 @@ const About: React.FC = () => {
       <h5 className="mb-4" >Hello! My name is</h5>
       <h3 className="css-typing-text mb-4" >Dmitrij Purynzin</h3>
       <h5 className="" >I'm a JavaScript developer. </h5>
-      <Button className="btn-outline-dark" style={{ marginTop: "20px" }} href="../../public/resume/Dmitrij_Purynzin_Resume.pdf" target="_blank">See My Resume</Button>
+      <Button className="btn-outline-dark" style={{ marginTop: "20px" }} href={pdf} target="_blank">See My Resume</Button>
     </div>
   );
 };
