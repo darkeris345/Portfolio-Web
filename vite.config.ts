087f97e2992f5039cb982f08d6ed59b53ad7a176
darkeris345 <dimuxxx72@gmail.com>
2024-03-20 10:18:55 +0200

    vite.config fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+import rawPlugin from 'vite-plugin-raw';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    rawPlugin({
+      
+      extensions: [".pdf"],
+    }),
+  ],
+});
