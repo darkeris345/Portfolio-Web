d80a85f5cf6eda0b7d0160ed7175a3dfdbacb08e
darkeris345 <dimuxxx72@gmail.com>
2024-03-20 09:04:34 +0200

    Upgrading skills part

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,60 +1,61 @@
 import React from "react";
 import "./Contact.scss";
-import image from "../../assets/pictures/man.png";
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 const Contact: React.FC = () => {
   return (
     <div className="contact" id="contact">
       <h2 className="m-5">Contact</h2>
-      <div className="d-flex ">
-        <img src={image} alt="dmitrij" style={{ width: "450px" }} />
-        <p className="nes-balloon from-left nes-pointer">
-          You can reach me easily by phone. My number is: +370 6757 6067
+      <div className="d-flex  justify-content-center">
+        <div className="nes-balloon nes-pointer img-fluid">
+          <p className="mt-4">Have any questions?</p>
+          <p>You can reach me easily by phone. My number is: +370 6757 6067</p>
           <div className="icon-list d-flex flex-column">
-            <div>
+            <div className="d-flex align-items-center ">
               <a
                 href="https://www.facebook.com/dmitrij.purynzin"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="contact-icon"
               >
-                <i className="nes-icon facebook is-large"></i>
+                <FontAwesomeIcon icon={faFacebook} size="3x" className="mx-5" />
               </a>
-              <span className="nes-text ml-2">Dmitrij Purynzin</span>
+              <span className="nes-text">Dmitrij Purynzin</span>
             </div>
-            <div>
+            <div className="d-flex align-items-center">
               <a
                 href="https://github.com/darkeris345"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="contact-icon"
               >
-                <i className="nes-icon github is-large"></i>
+                <FontAwesomeIcon icon={faGithub} size="3x" className="mx-5" />
               </a>
-              <span className="nes-text ml-2">darkeris345</span>
+              <span className="nes-text">darkeris345</span>
             </div>
-            <div>
+            <div className="d-flex align-items-center">
               <a
                 href="mailto:dmitrij.purynzin@gmail.com"
                 className="contact-icon"
               >
-                <i className="nes-icon gmail is-large"></i>
+                <FontAwesomeIcon icon={faEnvelope} size="3x" className="mx-5" />
               </a>
-              <span className="nes-text ml-2">dmitrij.purynzin@gmail.com</span>
+              <span className="nes-text">dmitrij.purynzin@gmail.com</span>
             </div>
-            <div>
+            <div className="d-flex align-items-center">
               <a
                 href="https://www.linkedin.com/in/dmitrij-purynzin/"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="contact-icon"
               >
-                <i className="nes-icon linkedin is-large"></i>
+                <FontAwesomeIcon icon={faLinkedin} size="3x" className="mx-5" />
               </a>
-              <span className="nes-text ml-2">Dmitrij Purynzin</span>
+              <span className="nes-text">Dmitrij Purynzin</span>
             </div>
           </div>
-        </p>
+        </div>
       </div>
     </div>
   );
--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
-import { FaReact, FaHtml5, FaCss3Alt, FaJs, FaNodeJs, FaSass  } from 'react-icons/fa';
-import { SiTypescript, SiNestjs } from 'react-icons/si';
-import './Skills.scss'; 
+import React from "react";
+import {
+  FaReact,
+  FaHtml5,
+  FaCss3Alt,
+  FaJs,
+  FaNodeJs,
+  FaSass,
+} from "react-icons/fa";
+import { SiTypescript, SiNestjs } from "react-icons/si";
+import "./Skills.scss";
 
 const Skills: React.FC = () => {
   return (
-    <div id="skills" className="m-5 container">
-      <h2 className='mb-5'>My Skills</h2>
+    <div
+      id="skills"
+      className="m-5 skills d-flex flex-column justify-content-center "
+    >
+      <h2 className="mb-5">My Skills</h2>
       <div className="icons-grid">
         <FaReact />
         <SiTypescript />
