5d7974c1006a73ab5afa37b854f2c168fcb4bb2c
darkeris345 <dimuxxx72@gmail.com>
2024-03-20 10:22:15 +0200

    Changing back 1 step

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
-import rawPlugin from 'vite-plugin-raw';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    rawPlugin({
-      extensions: [".pdf"],
-    }),
+    
   ],
 });
