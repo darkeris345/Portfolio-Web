690b7e54e68166ab1ce1bc82c4d9a9d7a9a144f0
darkeris345 <dimuxxx72@gmail.com>
2024-03-22 14:28:40 +0200

    Changed resume

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -8,29 +8,49 @@ import "aos/dist/aos.css";
 import { useEffect } from "react";
 
 const About: React.FC = () => {
-
   useEffect(() => {
     AOS.init();
   }, []);
   return (
     <div className="css-typing" id="about">
-      <img src={picture} alt="avatar" className="avatar" data-aos="zoom-out" data-aos-duration="1500" data-aos-delay="1500" />
-      <h5 className="mb-4" data-aos="fade-left" data-aos-duration="1500" data-aos-delay="1500">Hello! My name is</h5>
+      <img
+        src={picture}
+        alt="avatar"
+        className="avatar"
+        data-aos="zoom-out"
+        data-aos-duration="1500"
+        data-aos-delay="1500"
+      />
+      <h5
+        className="mb-4"
+        data-aos="fade-left"
+        data-aos-duration="1500"
+        data-aos-delay="1500"
+      >
+        Hello! My name is
+      </h5>
       <div className="typewriter-container">
         <Typewriter
           onInit={(typewriter) => {
             typewriter
               .pauseFor(3000)
               .typeString("Dmitrij Purynzin")
-              
+
               .start();
           }}
         />
       </div>
-      <h5 className="mt-4" data-aos="fade-right" data-aos-duration="1500" data-aos-delay="1500">I'm a passionate Web Developer </h5>
+      <h5
+        className="mt-4"
+        data-aos="fade-right"
+        data-aos-duration="1500"
+        data-aos-delay="1500"
+      >
+        I'm a passionate Web Developer{" "}
+      </h5>
       <Button
         className="nes-btn is-primary"
-        style={{ marginTop: "20px" }}
+        style={{ marginTop: "20px", marginBottom: "40px" }}
         href={pdf}
         target="_blank"
         data-aos="zoom-out-up"
